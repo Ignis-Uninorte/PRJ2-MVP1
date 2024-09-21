dadcea2c675e90009ab8916adffa848a575fdbf7
Tabata Llach <tllach@uninorte.edu.co>
2024-09-21 01:29:02 -0500

    feat(HU2013): plp initial content and styles added

--- src/pages/plp.tsx
+++ src/pages/plp.tsx
@@ -1,15 +1,40 @@
 import React from 'react';
-import {products} from "../utils/data";
+import "../styles/plp.css";
 import MainLayout from '../layouts/MainLayout';
-import ProductCard from "../components/ProductCard";
+import bannerImg from '../assets/banner-plp.jpg';
 
-
-
-const Plp: React.FC = () => {
+const Plp: React.FC = () =>{
     return (
         <MainLayout>
-            <div>
-
+            <div className="grid-container">
+                <nav className="breadcrumb">
+                    <p>BREADCRUM</p>
+                    {/* Añadir cuando este listo en el otro punto de historia */}
+                </nav>
+                <section className="information-above">
+                    <p>Showing 1-5 of 5 results</p>
+                    <div className="orderby">
+                        <h4>Order By</h4>
+                        <form>
+                            {/* Añadir funcionalidad */}
+                            <select id="orderby-box">
+                                <option value="relevance">Relevance</option>
+                                <option value="date">Date</option>
+                                <option value="pricel-h">Price lower to higher</option> 
+                                <option value="priceh-l">Price higher to lower</option>
+                            </select>
+                        </form>
+                    </div>
+                </section> 
+                <aside className="filters">
+                    <h2>Filters</h2>
+                    {/* Añadir cuando este listo en el otro punto de historia */}
+                </aside>
+                <section className="content">
+                </section>
+                <div className="banner">
+                    <img src={bannerImg} alt="Banner"></img>
+                </div>
             </div>
         </MainLayout>
         );
