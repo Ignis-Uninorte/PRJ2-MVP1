d728525e7e85a4980c28681d4b574d3d7e2d146c
Tabata Llach <tllach@uninorte.edu.co>
2024-09-21 22:49:48 -0500

    feat(HU2013): function and implementation to add dinamic star review added

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,10 +1,30 @@
 import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faStar, faStarHalf} from '@fortawesome/free-solid-svg-icons'
 import {ProductItem} from "../types/plp.type";
 import "../styles/productCard.css";
 
 function getDiscountPrice(price: number, discount: number): number{
     return Number((price - (price * discount / 100)).toFixed(2));
 }
+
+// Función para generar las estrellas según el rating
+const renderStars = (rating: number) => {
+    const stars = [];
+    const fullStars = Math.floor(rating);
+    const halfStar = rating % 1 !== 0; // Determina si hay media estrella
+    for (let i = 0; i < fullStars; i++) {
+        stars.push(
+            <FontAwesomeIcon key={i} icon={faStar} style={{ color: "#FFD43B" }} />
+        );
+    }
+    if (halfStar) {
+        stars.push(
+            <FontAwesomeIcon key="half" icon={faStarHalf} style={{ color: "#FFD43B" }} />
+        );
+    }
+    return stars;
+};
 
 interface ProductCardProps {
     product: ProductItem;
@@ -17,8 +37,8 @@ const ProductCard: React.FC<ProductCardProps> = ({product}) =>{
             <img src={product.image} alt={product.name} />
             <h3>{product.name}</h3>
             <p className="short-description">{product.description}</p>
-            <div className="review">
-                <i className="fa-solid fa-star"></i>5
+            <div>
+                {renderStars(product.rating)}
             </div>
             <p className="normal-price"><s>${product.price}</s></p>
             <div className="price-discount">
