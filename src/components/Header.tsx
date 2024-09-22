fb7409ea943abc144e94443a8a5aa77f93331ec0
Camilo Heras <caheras@uninorte.edu.co>
2024-09-21 20:38:07 -0500

    Refactor header component and styles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,18 +4,42 @@ import '../styles/subheader.css';
 
 const Header: React.FC = () => {
   return (
-    <div>
-      {/* Header */}
-      <header className="header">
-        <h1>Header</h1>
-      </header>
+    <header>
+      <div className="header-container">
+        {/* Logo */}
+        <div className="logo">
+          <img src="src\assets\Ignis_Store_Logo.png" alt="Company Logo" />
+        </div>
 
+        {/* Search Bar */}
+        <div className="search-bar">
+          <input type="text" placeholder="Search" />
+          <button className="search-button">
+            <img src="src/assets/search_icon.svg" alt="Search" />
+          </button>
+        </div>
+
+        {/* Account and Cart Links */}
+        <div className="account-cart">
+          <a className="account-link" href="/account">
+            <img src="src/assets/account-icon.svg" alt="My Account" />
+            <span>My Account</span>
+          </a>
+          <div className="divider"></div>
+          <a className="cart-link" href="/cart">
+            <img src="src/assets/cart-icon.svg" alt="Cart" />
+            <span>My Cart</span>
+          </a>
+        </div>
+      </div>
       {/* Subheader */}
       <div className="subheader">
         <h2>Subheader</h2>
-      </div>
-    </div>
+      </div>      
+    </header>
+    
   );
+  
 };
 
 export default Header;
