ba8c1e9037e1b7e05ed2a1c04a0225565525b15b
Jcoll05 <jcolla@uninorte.edu.co>
2024-09-22 18:28:18 -0500

    Fixed misalignment with the feature products appearing after footer banner.

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -18,15 +18,14 @@ const Home: React.FC = () => {
         imageUrl="src\assets\Discounted_product2.jpg" 
         altText="Discount Image 2"
       />
-        <p>Content section (Main)</p>
+      <BrowserRouter>
+        <FeaturedProducts />
+      </BrowserRouter> 
         <Banner 
         imageUrl="src\assets\banner_img.gif" 
         altText="Banner"
       />
       </div>
-      <BrowserRouter>
-        <FeaturedProducts />
-      </BrowserRouter> 
     </MainLayout>
   );
 };
