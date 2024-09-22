67b532d0b26fe17f04ae9764d7fe6151952e0fb6
gdgalvis <73660713+gdgalvis@users.noreply.github.com>
2024-09-21 20:40:40 -0500

    feature(2010)
    add promotional images

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import MainLayout from '../layouts/MainLayout';
 import Banner from '../components/Banner';
+import PImg from '../components/PImg';
 
 const Home: React.FC = () => {
   return (
     <MainLayout>
       <div>
+      <PImg 
+        imageUrl="src\assets\Discounted_product1.jpg" 
+        altText="Discount Image 1"
+      />
+      <PImg 
+        imageUrl="src\assets\Discounted_product2.jpg" 
+        altText="Discount Image 2"
+      />
         <p>Content section (Main)</p>
         <Banner 
         imageUrl="src\assets\banner_img.gif" 
