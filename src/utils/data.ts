5306cd9be8664e36718d2752a7837ae0768a12aa
Tabata Llach <tllach@uninorte.edu.co>
2024-09-21 22:52:21 -0500

    feat(HU2013): url for computer section in array added

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -5,27 +5,27 @@ export const products = [
         items: [
             {
                 id: 1,
-                name: "Samsung Galaxy Tab S9 FE+ Plus 12.4” 128GB Android Tablet",
+                name: "Samsung Galaxy Tab S9 FE+ Plus",
                 description: "New tablet Samsung with 128 GB. Display Size of 12.4 Inches",
                 price: 599.99,
                 discount: 20,
-                image: "../assets/products/tablets/samsung.jpg",
+                image: "https://www.alkosto.com/medias/8806095160689-001-750Wx750H?context=bWFzdGVyfGltYWdlc3w4OTUyfGltYWdlL3dlYnB8YUdGaEwyZzRaUzh4TkRZMU1qWXpNelkzTnpnMU5DODRPREEyTURrMU1UWXdOamc1WHpBd01WODNOVEJYZURjMU1FZ3xhZTY1MDQxYTkwZjNkZjIyMDU5NzZhMjhhMWFhZDFmYjdjYzIzZmFhYWNmMWZmNjhmMWJkOGM4Zjk3Y2Y0Mjky",
                 rating: 5,
                 especifications: {
                     brand: "Samsung",
                     condition: "New",
                     memoryStorage: "128 GB",
                     displaySize: "12'4 Inches",
-                    color: "Black",
+                    color: "Mint",
                 }
             },
             {
                 id: 2,
-                name: "SAMSUNG Galaxy Tab A9+ Plus 11” 64GB Android Tablet",
+                name: "SAMSUNG Galaxy Tab A9+ Plus",
                 description: "Renew tablet Samsung with 64 GB. Display Size of 11 Inches",
                 price: 150.99,
                 discount: 20,
-                image: "../assets/products/tablets/rp1.jpg",
+                image: "https://www.alkosto.com/medias/8806095574967-002-750Wx750H?context=bWFzdGVyfGltYWdlc3wyOTIzOHxpbWFnZS93ZWJwfGFHUmhMMmd3WXk4eE5EUXhNRGN6T0RRMk5qZzBOaTg0T0RBMk1EazFOVGMwT1RZM1h6QXdNbDgzTlRCWGVEYzFNRWd8NzQ1NDRiZTAzZDcwYjFmMmM5YTliZWVjODU5NzgwNzg5ZDBjYjEzZThkNTZiNWM3MDM0ZDY4OTJkN2UzNmNiZA",
                 rating: 4.5,
                 especifications: {
                     brand: "Samsung",
@@ -41,7 +41,7 @@ export const products = [
                 description: "Used Android tablet with 16GB. Display Size of 11 Inches",
                 price: 89.99,
                 discount: 10,
-                image: "../assets/products/tablets/rp2.jpg",
+                image: "https://www.alkosto.com/medias/4894461992846-001-750Wx750H?context=bWFzdGVyfGltYWdlc3wzNDk1MnxpbWFnZS93ZWJwfGFEQTRMMmhoTXk4eE5EUTVPRGc1TXpneU5EQXpNQzgwT0RrME5EWXhPVGt5T0RRMlh6QXdNVjgzTlRCWGVEYzFNRWd8ZWMyYzc5OWQ2ZDA5MDgyYTVmZDRiZmFhNjA1N2NkYjg0YzliMjEzYmYwYzFkNTBkZGZmMjk1Mzg3OWM1MTY5Nw",
                 rating: 2,
                 especifications: {
                     brand: "Generic",
@@ -53,14 +53,14 @@ export const products = [
             },
             {
                 id: 4,
-                name: "Amazon Fire HD 10 tablet",
+                name: "Lenovo Fire HD 10 tablet",
                 description: "Used Android tablet with 8GB. Display Size of 10 Inches",
                 price: 289.99,
                 discount: 35,
-                image: "../assets/products/tablets/rp3.jpg",
+                image: "https://img01.huaweifile.com/sg/ms/co/pms/uomcdn/CO_HW_B2C/pms/202310/gbom/6942103103476/800_800_A601DE9795C7D47430A8E35A0C53F93Dmp.png",
                 rating: 4.2,
                 especifications: {
-                    brand: "Amazon",
+                    brand: "Lenovo",
                     condition: "Used",
                     memoryStorage: "8 GB",
                     displaySize: "10 Inches",
@@ -73,7 +73,7 @@ export const products = [
                 description: "New Lenovo tab M9 with 64GB. Display Size of 9 Inches",
                 price: 189.99,
                 discount: 58,
-                image: "../assets/products/tablets/rp4.jpg",
+                image: "https://www.alkosto.com/medias/197530283857-001-750Wx750H?context=bWFzdGVyfGltYWdlc3wyMjI2MnxpbWFnZS93ZWJwfGFEVTFMMmd6WVM4eE5EVTJNekkzTmpBNU5UVXhPQzh4T1RjMU16QXlPRE00TlRkZk1EQXhYemMxTUZkNE56VXdTQXw1OGE2N2ZjMzBjOTMzZWVkMzNkNzNiNTY4NGY5NzU2NjZlMGE3ODc4ODcwNzQ4M2YxMmJiYWU3YmFhYmFmN2I5",
                 rating: 4,
                 especifications: {
                     brand: "Lenovo",
