76876211637c82f0a9601495c048abd07f398ed2
Jcoll05 <jcolla@uninorte.edu.co>
2024-10-02 11:08:26 -0500

    moved services that belong on other feature branch.

--- src/services/featured.services.ts
+++
@@ -1,9 +0,0 @@
-import apiManager from './api';
-
-export function getPromotionalImages() {
-  return apiManager.get('/featured')
-    .then(response => response.data)
-    .catch(error => {
-      throw error;
-    });
-}
