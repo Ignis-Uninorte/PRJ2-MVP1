435b9b3eb895d144dd62f6e68ecf6a000c2ba2e4
Tabata Llach <tllach@uninorte.edu.co>
2024-10-02 22:31:18 -0500

    feat(HU2023): final style touches finished

--- src/components/ProductDetail.tsx
+++ src/components/ProductDetail.tsx
@@ -1,13 +1,22 @@
-import {useGetProducts} from '../hooks/useGetProduct'
+import {useGetProducts} from '../hooks/useGetProduct';
+import '../styles/productDetail.css';
+import {ProductItem} from '../types/plp.type' 
 
 interface ProductDetailProps {
     idProduct: string,
     category: string
 }
 
-
 const ProductDetail: React.FC<ProductDetailProps> = ({idProduct, category})  => {
     const {isLoading, isSuccess, isError, data: products} = useGetProducts();
+    const productCategory = products?.find(product => product.category === category);
+    const product = productCategory?.products.find(
+        (product: ProductItem) => product.id === Number(idProduct)
+    )
+    const priceDiscount = Number(
+        ((product?.price ?? 0) - (product?.price ?? 0) * (product?.discount ?? 0) / 100).toFixed(2)
+    );    
+
     return (
         <div>
             {isLoading && (
@@ -18,10 +27,24 @@ const ProductDetail: React.FC<ProductDetailProps> = ({idProduct, category})  => 
             )}
             {!isLoading && !isError && isSuccess && products && (
                 <div className="product-layout">
-                    { productCategory = products.find(product => product.category === category);}
-                    <p>{idProduct}</p>
-                    <p>{category}</p>
-                    <p>Aca va la info del producto</p>
+                    <img src={product?.image} alt="Product Image" className="img-product"/>
+                    <section className="product-info">
+                        <section className="product-details">
+                            <h1>{product?.name}</h1>
+                            <div className="price">
+                                <p className="normal-price"><s>${product?.price}</s></p>
+                                <div className="price-discount">
+                                    <p>${priceDiscount}</p>
+                                    <p className="discount">({product?.discount}% off)</p>
+                                </div>
+                            </div>
+                            <ul>
+                                {product?.description.map(
+                                    desc => <li><strong>{desc.title} </strong>{desc.description}</li>
+                                )}
+                            </ul>
+                        </section>
+                    </section>
                 </div>
             )}
         </div>
