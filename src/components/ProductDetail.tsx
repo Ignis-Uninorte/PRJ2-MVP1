736954c2edb788fc6742a43e03290f9746f028b8
ana03ap <anxrdilx03@gmail.com>
2024-10-04 11:16:27 -0500

    fixing css

--- src/components/ProductDetail.tsx
+++ src/components/ProductDetail.tsx
@@ -3,8 +3,6 @@ import '../styles/productDetail.css';
 import {ProductItem} from '../types/plp.type' 
 import LoadingSpinner from '../components/Spinner';
 import ErrorComponent from '../components/Error';
-import Breadcrumb from '../components/Breadcrumb';
-
 
 interface ProductDetailProps {
     idProduct: string,
@@ -31,7 +29,6 @@ const ProductDetail: React.FC<ProductDetailProps> = ({idProduct, category})  => 
             )}
             {!isLoading && !isError && isSuccess && products && (
                 <div className="product-layout">
-                    <Breadcrumb category={category} productName={product?.name} />
                     <div className="image-container-pd">
                         <img src={product?.image} alt="Product Image" className="img-product"/>
                     </div>
@@ -50,6 +47,9 @@ const ProductDetail: React.FC<ProductDetailProps> = ({idProduct, category})  => 
                                     desc => <li><strong>{desc.title} </strong>{desc.description}</li>
                                 )}
                             </ul>
+                            <button type="button">
+                                <i className="fa-solid fa-cart-shopping"></i>Add to cart
+                            </button>
                         </section>
                     </section>
                 </div>
